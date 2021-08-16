1ceeb986a05e30acadc4100e68be6b61a7e3ca6c
abdullah <bido6573@gmail.com>
2021-08-16 03:44:28 +0200

    4th commit for E-commerce app after a trial to attach the headlines with the fragments

--- app/src/main/java/com/abdullah/e_commerce/MainActivity.java
+++ app/src/main/java/com/abdullah/e_commerce/MainActivity.java
@@ -3,11 +3,16 @@ package com.abdullah.e_commerce;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.navigation.NavController;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.abdullah.e_commerce.databinding.ActivityMainBinding;
 import com.google.android.material.badge.BadgeDrawable;
@@ -30,7 +35,30 @@ public class MainActivity extends AppCompatActivity {
 
         binding.mainActivityVp.setAdapter(new PagerAdapter(MainActivity.this));
 
+        binding.mainActivityLoginTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.mainActivitySignupTv.setVisibility(View.GONE);
+                binding.mainActivityLoginTv.setTextColor(Color.BLACK);
+                binding.mainActivitySignupTv.setVisibility(View.VISIBLE);
+                binding.mainActivitySignupTv.setLeft(View.FOCUS_LEFT);
+//                binding.mainActivityLl.removeView(binding.mainActivitySignupTv);
+//                binding.mainActivityLl.addView(binding.mainActivitySignupTv, 40, 33);
+            }
+        });
 
+        binding.mainActivityForgotPasswordTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.mainActivitySignupTv.setVisibility(View.GONE);
+                binding.mainActivityLoginTv.setVisibility(View.GONE);
+                binding.mainActivityForgotPasswordTv.setTextColor(Color.BLACK);
+                binding.mainActivitySignupTv.setLeft(View.FOCUS_LEFT);
+                binding.mainActivityLoginTv.setLeft(View.FOCUS_LEFT);
+//                binding.mainActivityLl.removeView(binding.mainActivitySignupTv);
+//                binding.mainActivityLl.addView(binding.mainActivitySignupTv, 40, 33);
+            }
+        });
 
     }
 }
