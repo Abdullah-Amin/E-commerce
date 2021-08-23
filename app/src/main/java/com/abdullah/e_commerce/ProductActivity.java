ee7b78401e2314e2171d28cecf63d1bd5e7d9db1
abdullah <bido6573@gmail.com>
2021-08-23 03:42:52 +0200

    18th commit for E-commerce app after making 2 of ProductFragments (ProductFragment, DetailsFragment)

--- app/src/main/java/com/abdullah/e_commerce/ProductActivity.java
+++ app/src/main/java/com/abdullah/e_commerce/ProductActivity.java
@@ -1,14 +1,45 @@
 package com.abdullah.e_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+
+import com.abdullah.e_commerce.databinding.ActivityProductBinding;
 
 public class ProductActivity extends AppCompatActivity {
+
+    ActivityProductBinding binding;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_product);
+
+        binding.activityProductProductMbtn.setBackgroundColor(Color.WHITE);
+        binding.activityProductProductMbtn.setTextColor(Color.RED);
+
+        binding.activityProductDetailsMbtn.setBackgroundColor(Color.TRANSPARENT);
+        binding.activityProductDetailsMbtn.setTextColor(Color.GRAY);
+
+        binding.activityProductReviewsMbtn.setBackgroundColor(Color.TRANSPARENT);
+        binding.activityProductReviewsMbtn.setTextColor(Color.GRAY);
+
+        binding.activityProductProductMbtn.setOnClickListener(v -> {
+            binding.activityProductProductMbtn.setBackgroundColor(Color.WHITE);
+            binding.activityProductProductMbtn.setTextColor(Color.RED);
+
+            binding.activityProductDetailsMbtn.setBackgroundColor(Color.TRANSPARENT);
+            binding.activityProductDetailsMbtn.setTextColor(Color.GRAY);
+
+            binding.activityProductReviewsMbtn.setBackgroundColor(Color.TRANSPARENT);
+            binding.activityProductReviewsMbtn.setTextColor(Color.GRAY);
+
+        });
     }
 }
