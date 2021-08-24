ee41bd56ead6ab85082f943d52ec06ee4162f127
abdullah <bido6573@gmail.com>
2021-08-24 04:26:19 +0200

    20th commit for E-commerce app after making CartFragment

--- app/src/main/java/com/abdullah/e_commerce/ProductActivity.java
+++ app/src/main/java/com/abdullah/e_commerce/ProductActivity.java
@@ -10,11 +10,11 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.abdullah.e_commerce.databinding.ActivityProductBinding;
+import com.google.android.material.button.MaterialButton;
 
 public class ProductActivity extends AppCompatActivity {
 
     ActivityProductBinding binding;
-    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +30,8 @@ public class ProductActivity extends AppCompatActivity {
         binding.activityProductReviewsMbtn.setBackgroundColor(Color.TRANSPARENT);
         binding.activityProductReviewsMbtn.setTextColor(Color.GRAY);
 
+
+
         binding.activityProductProductMbtn.setOnClickListener(v -> {
             binding.activityProductProductMbtn.setBackgroundColor(Color.WHITE);
             binding.activityProductProductMbtn.setTextColor(Color.RED);
@@ -39,7 +41,29 @@ public class ProductActivity extends AppCompatActivity {
 
             binding.activityProductReviewsMbtn.setBackgroundColor(Color.TRANSPARENT);
             binding.activityProductReviewsMbtn.setTextColor(Color.GRAY);
-
         });
+
+        binding.activityProductDetailsMbtn.setOnClickListener(v -> {
+            binding.activityProductDetailsMbtn.setBackgroundColor(Color.WHITE);
+            binding.activityProductDetailsMbtn.setTextColor(Color.RED);
+
+            binding.activityProductProductMbtn.setBackgroundColor(Color.TRANSPARENT);
+            binding.activityProductProductMbtn.setTextColor(Color.GRAY);
+
+            binding.activityProductReviewsMbtn.setBackgroundColor(Color.TRANSPARENT);
+            binding.activityProductReviewsMbtn.setTextColor(Color.GRAY);
+        });
+
+        binding.activityProductReviewsMbtn.setOnClickListener(v -> {
+            binding.activityProductReviewsMbtn.setBackgroundColor(Color.WHITE);
+            binding.activityProductReviewsMbtn.setTextColor(Color.RED);
+
+            binding.activityProductDetailsMbtn.setBackgroundColor(Color.TRANSPARENT);
+            binding.activityProductDetailsMbtn.setTextColor(Color.GRAY);
+
+            binding.activityProductProductMbtn.setBackgroundColor(Color.TRANSPARENT);
+            binding.activityProductProductMbtn.setTextColor(Color.GRAY);
+        });
+
     }
 }
--- app/src/main/java/com/abdullah/e_commerce/ReviewsFragment.java
+++ app/src/main/java/com/abdullah/e_commerce/ReviewsFragment.java
@@ -2,18 +2,37 @@ package com.abdullah.e_commerce;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.abdullah.e_commerce.databinding.FragmentReviewsBinding;
+
+import org.jetbrains.annotations.NotNull;
+
 public class ReviewsFragment extends Fragment {
 
+    FragmentReviewsBinding binding;
+    NavController navController;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reviews, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_reviews, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 }
