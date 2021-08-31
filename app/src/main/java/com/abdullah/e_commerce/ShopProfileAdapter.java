d321eb8664ed72c6b0fbe6001331a9bfc0e327da
abdullah <bido6573@gmail.com>
2021-08-31 19:13:06 +0200

    29th commit for E-commerce app after making ShopProfileActivity

---
+++ app/src/main/java/com/abdullah/e_commerce/ShopProfileAdapter.java
@@ -0,0 +1,31 @@
+package com.abdullah.e_commerce;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ShopProfileAdapter extends FragmentStateAdapter {
+
+    public ShopProfileAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch(position){
+            case 0:
+                return new ShopHomeFragment();
+            case 1:
+                return new ShopProductFragment();
+            default:
+                return new ShopReviewsFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
