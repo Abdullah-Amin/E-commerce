3bdcc8fa211f4ed89d47f62d4ce34380247f867c
abdullah <bido6573@gmail.com>
2021-09-04 17:28:21 +0200

    35th commit for E-commerce app after almost finished all the design

---
+++ app/src/main/java/com/abdullah/e_commerce/OrderPlacedActivity.java
@@ -0,0 +1,14 @@
+package com.abdullah.e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderPlacedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_placed);
+    }
+}
